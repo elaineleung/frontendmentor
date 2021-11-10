298b4ecfd91550d5aa22272ae1232774e2d6e598
Elaine L <dev@elalun.com>
2021-11-10 00:39:30 -0800

    Add Ping coming soon challenge

--- pingcomingsoonpage/script.js
+++ pingcomingsoonpage/script.js
@@ -1,34 +1,31 @@
 const formEl = document.getElementById("formEl");
 const signupEl = document.getElementById("signupEl");
-const inputs = [...document.querySelectorAll('.form__input')]
+const emailEl = document.getElementById("emailEl");
 
-formEl.addEventListener('submit', ()=>{
+const input = emailEl.querySelector("input");
+const message = emailEl.querySelector(".input__message");
+
+formEl.addEventListener("submit", () => {
   signupEl.innerHTML = `
         <h3 class="submit__title">Thank you for signing up!</h3>
         <p class="submit__message">Check your email to confirm your signup.</p>
     `;
-})
+});
 
-inputs.forEach(inputDiv => {
-  const input = inputDiv.querySelector("input")
-  const message = inputDiv.querySelector(".input__message")
-  
-  input.addEventListener("invalid", (event) => {
-    event.preventDefault()
-    input.classList.add("is-invalid")
-    if(event.target.validity.valueMissing){
-      message.textContent = `Your ${input.name} cannot be empty`
-    }
-    if(event.target.validity.patternMismatch){
-      message.textContent = `Please provide a valid ${input.name}`
-    }
-    if(event.target.validity.tooShort){
-      message.textContent = `Your ${input.name} needs to be longer than ${input.minLength} characters`
-    }
+input.addEventListener("invalid", (event) => {
+  event.preventDefault();
+  input.classList.add("is-invalid");
+  if (event.target.validity.valueMissing) {
+    message.textContent = `Your ${input.name} cannot be empty`;
+  }
+  if (event.target.validity.patternMismatch) {
+    message.textContent = `Please provide a valid email address`;
+  }
+  if (event.target.validity.tooShort) {
+    message.textContent = `Your ${input.name} needs to be longer than ${input.minLength} characters`;
+  }
+});
 
-  })    
-  input.removeEventListener("valid", (event) => {
-    input.classList.remove("is-invalid")
-  })
-})
-
+input.removeEventListener("valid", (event) => {
+  input.classList.remove("is-invalid");
+});
