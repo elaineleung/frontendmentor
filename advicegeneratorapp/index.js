288d8145d338a0287f4992ff6572812841524bb6
Elaine L <dev@elalun.com>
2022-07-29 06:25:17 -0700

    Update CSS

--- advicegeneratorapp/index.js
+++ advicegeneratorapp/index.js
@@ -1,21 +1,36 @@
+const componentEl = document.getElementById("compEl")
 const numEl = document.getElementById("numEl")
 const adviceEl = document.getElementById("adviceEl")
 const diceEl = document.getElementById("diceEl")
+const loadingEl = document.getElementById("loadingEl")
 
 const url = "https://api.adviceslip.com/advice"
 const max = 224  // max number of quotes from endpoint
 
 loadAdvice()
+// loadingEl.setAttribute("aria-hidden", true)
 
 diceEl.addEventListener("click", loadAdvice)
 
 function loadAdvice() {
+  reset()
+  loadingEl.setAttribute("aria-hidden", true)
   const random = Math.floor(Math.random() * max)
- 
+
   fetch(`${url}/${random}`)
     .then( response => response.json())
     .then( data => {
-      numEl.textContent = `Advice #${data.slip.id}`
-      adviceEl.textContent = `“${data.slip.advice}”`
+      if (data) {
+        setTimeout(()=> {
+          loadingEl.setAttribute("aria-hidden", false)
+          numEl.textContent = `Advice #${data.slip.id}`
+          adviceEl.textContent = `“${data.slip.advice}”`
+        }, 1000)
+      } 
    })
+}
+
+function reset() {  
+  numEl.textContent = ""
+  adviceEl.textContent = ""
 }
