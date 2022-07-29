3ed187ed71655d5a563e2cd0804e787b0cd3be9e
Elaine L <dev@elalun.com>
2022-07-29 04:05:34 -0700

    Update CSS

--- expenseschartcomponent/index.js
+++ expenseschartcomponent/index.js
@@ -2,33 +2,50 @@
 const barsEl = document.getElementById("barsEl")
 
 const currentDay = new Date().getDay()
+let barWidth;
 
-fetch("./data.json")
-.then(response => {
-   return response.json();
-})
-.then(data => {
-  
-  data.forEach( (info, idx) => {
-    const dayDiv = document.createElement("div")
-    const amountDiv = document.createElement("div")
-    const barDiv = document.createElement("div")
-    const labelDiv = document.createElement("p")
-    
-    dayDiv.classList.add('bars__day')
+getData()
+window.addEventListener('resize', getData)
 
-    amountDiv.classList.add('bars__day-amount')
-    amountDiv.textContent = `$${info.amount}`
-    
-    barDiv.classList.add('bars__day-bar')
-
-    idx + 1 === currentDay && barDiv.classList.add('highlight')
-    barDiv.style.height = `${info.amount * 0.179}rem`
-
-    labelDiv.classList.add('bars__day-label')  
-    labelDiv.textContent = info.day
-
-    barsEl.appendChild(dayDiv)
-    dayDiv.append(barDiv, amountDiv, labelDiv)  
+function getData() {
+  fetch("./data.json")
+  .then(response => {
+     return response.json();
   })
-});
+  .then(data => {
+    barsEl.textContent = ''
+    data.forEach( (info, idx) => {
+      const dayDiv = document.createElement("div")
+      const amountDiv = document.createElement("div")
+      const barDiv = document.createElement("div")
+      const labelDiv = document.createElement("p")
+      
+      dayDiv.classList.add('bars__day')
+  
+      amountDiv.classList.add('bars__day-amount')
+      amountDiv.textContent = `$${info.amount}`
+  
+      barDiv.classList.add('bars__day-bar')
+  
+      idx + 1 === currentDay && barDiv.classList.add('highlight')
+      barDiv.style.height = `${info.amount * 0.179}rem`
+  
+      labelDiv.classList.add('bars__day-label')  
+      labelDiv.textContent = info.day
+
+      barsEl.appendChild(dayDiv)
+      dayDiv.append(barDiv, amountDiv, labelDiv)  
+  
+      !barWidth && getBarWidth(idx, barDiv.clientWidth)
+  
+      const margin = ((amountDiv.clientWidth - barWidth) / 2)
+      amountDiv.setAttribute('data-width', `-${margin}`)  
+     
+    })
+  });
+}
+
+function getBarWidth(idx, width) {
+  const gap = window.innerWidth >= 980 ? 18.4 : 12.48
+  barWidth = idx === 0 && ((width - ((6 - idx) * gap)) / (7 - idx))
+}
