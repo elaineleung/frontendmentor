98324646c589cf6e7e283c612c2bb3b3c1d4dbc5
Elaine L <dev@elalun.com>
2022-08-22 00:58:31 -0700

    Add Blogr

---
+++ timetrackingdashboard/helpers.js
@@ -0,0 +1,14 @@
+export const areasMap = {
+  work: "Work",
+  play: "Play",
+  study: "Study",
+  exercise: "Exercise",
+  social: "Social",
+  "self-care": "Self Care",
+};
+
+export const timeFrameMap = {
+  daily: "Yesterday",
+  weekly: "Last Week",
+  monthly: "Last Month",
+};
--- timetrackingdashboard/index.js
+++ timetrackingdashboard/index.js
@@ -1,46 +1,39 @@
-const cardsEls = document.querySelectorAll(".card");
-const timeframeEls = document.querySelectorAll("[type='radio']");
+import { areasMap, timeFrameMap } from "./helpers.js"
 
 let selected = "weekly";
-let userData;
-
-const areasMap = {
-  work: "Work",
-  play: "Play",
-  study: "Study",
-  exercise: "Exercise",
-  social: "Social",
-  "self-care": "Self Care",
-};
-
-const timeFrameMap = {
-  daily: "Yesterday",
-  weekly: "Last Week",
-  monthly: "Last Month",
-};
 
 // first fetch data
-
-fetch("./data.json")
+function initialize(){
+  let userData;
+  const timeframeEls = document.querySelectorAll("[type='radio']");
+  
+  fetch("./data.json")
   .then((response) => response.json())
   .then((data) => {
     userData = data;
     updateValues(userData);
+    handleTimeframes(timeframeEls, userData)
     const selectedEl = [...timeframeEls].find( el => el.value === selected)
     selectedEl.checked = true;
   })
   .catch((error) => console.log(error));
+}
+initialize()
 
- // event listener for each button
+ // event listener for each timeframe button
 
-timeframeEls.forEach((timeframe) => {
-  timeframe.addEventListener("click", (event) => {
-    selected = event.target.value;
-    updateValues(userData);
+function handleTimeframes(timeframes, data){
+  timeframes.forEach((timeframe) => {
+    timeframe.addEventListener("click", (event) => {
+      selected = event.target.value;
+      updateValues(data);
+    });
   });
-});
+}
 
 function updateValues(data) {
+  const cardsEls = document.querySelectorAll(".card");
+
   cardsEls.forEach((card) => {
     // find the dataset that matches the card's life area
     const stats = data.find((item) => item.title === areasMap[card.id]);
@@ -53,8 +46,6 @@ function updateValues(data) {
       return `${num}${num === 1 ? "hr" : "hrs"}`;
     }
     statsNow.textContent = displayHour(current);
-    statsPrev.textContent = `${timeFrameMap[selected]} - ${displayHour(
-      previous
-    )}`;
+    statsPrev.textContent = `${timeFrameMap[selected]} - ${displayHour(previous)}`;
   });
 }
