65bfda552085b894e019bae8138ccac5dae5435f
Elaine Leung <dev@elalun.com>
2022-12-12 13:55:46 -0800

    Update Advice app CSS

--- advicegeneratorapp/index.js
+++ advicegeneratorapp/index.js
@@ -18,26 +18,26 @@ diceEl.addEventListener("keydown", (event)=>{
 })
  
 async function loadAdvice() {
-  loadingEl.setAttribute("aria-hidden", true) 
+  loadingEl.setAttribute("aria-hidden", false) 
 
   try {
     const response = await fetch(`${url}`, { cache: 'no-cache' })
     const data = await response.json()
     if (!data) {        
       setTimeout(()=> {
-        loadingEl.setAttribute("aria-hidden", false)
+        loadingEl.setAttribute("aria-hidden", true)
         componentEl.querySelector(".advice__text").textContent = `Sorry, no advice right now, but please check again later!`
       }, 1000)      
     } else {
       setTimeout(()=> {
-        loadingEl.setAttribute("aria-hidden", false)
+        loadingEl.setAttribute("aria-hidden", true)
         componentEl.querySelector(".advice__number").textContent = `Advice #${data.slip.id}`
-        componentEl.querySelector(".advice__text").textContent = `“${data.slip.advice}”`
+        componentEl.querySelector(".advice__text").innerHTML = `“${data.slip.advice}”`
         document.body.focus();
       }, 1000)
     }
   } catch(error) {
-    console.log("Error caught: ", error);
+    console.log("Error caught:", error);
   }
 }
  
